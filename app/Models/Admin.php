678cdecfd2d6c09c326f140dea328bcac3e1c364
Brain Barett <brianbarett97@gmail.com>
2023-08-05 19:08:25 -0400

    feat: admin entity

---
+++ app/Models/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, HasFactory;
+
+	protected $table = 'admin';
+    protected $guarded = [];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+	protected function password(): Attribute
+	{
+		return Attribute::make(
+			set: fn(string $password) => Hash::make($password)
+		);
+	}
+}
--- database/migrations/2014_10_12_000000_create_admin_table.php
+++ database/migrations/2014_10_12_000000_create_admin_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admin');
     }
 };
