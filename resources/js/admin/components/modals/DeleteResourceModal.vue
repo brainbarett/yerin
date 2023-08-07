0a262190b219be9da5217f3a864afcd45d3895e6
Brain Barett <brianbarett97@gmail.com>
2023-08-07 15:25:34 -0400

    [FE]feat: modals & delete-resource-modals

---
+++ resources/js/admin/components/modals/DeleteResourceModal.vue
@@ -0,0 +1,60 @@
+<template>
+	<Modal @close="$emit('close')" :title="title">
+		<div class="flex flex-col gap-4">
+			<slot />
+
+			<div class="mt-5">
+				<p class="mb-1 text-sm">Please type 'confirm' to delete</p>
+
+				<div class="relative inline-block h-[42px]">
+					<input
+						v-model="confirmation"
+						type="text"
+						class="box-border px-3 py-2 bg-white border border-gray-300 rounded"
+						:disabled="loading"
+					/>
+					<button
+						@click="confirm()"
+						type="button"
+						class="box-border absolute h-8 px-3 py-1 text-white -translate-y-1/2 bg-red-500 rounded top-1/2 right-1"
+						:disabled="loading"
+					>
+						<loading-spinner v-if="loading" size="s" />
+						<template v-else>DELETE</template>
+					</button>
+				</div>
+			</div>
+		</div>
+	</Modal>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+	import Modal from './Modal.vue'
+
+	export default Vue.extend({
+		components: { Modal },
+
+		props: {
+			title: String,
+			loading: {
+				type: Boolean,
+				default: false,
+			},
+		},
+
+		data() {
+			return {
+				confirmation: '' as string,
+			}
+		},
+
+		methods: {
+			confirm() {
+				if (this.confirmation.toLowerCase() == 'confirm') {
+					this.$emit('confirm')
+				}
+			},
+		},
+	})
+</script>
---
+++ resources/js/admin/components/modals/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+	<div
+		class="box-border absolute top-0 left-0 z-10 flex items-center justify-center w-full h-full p-8"
+		style="background: rgba(0, 0, 0, 0.3)"
+	>
+		<div
+			class="box-border flex flex-col gap-4 p-4 bg-white rounded w-[700px]"
+			style="max-width: calc(100% - 32px)"
+		>
+			<header class="flex items-center">
+				<span class="font-medium">{{ title }}</span>
+
+				<button @click="$emit('close')" type="button" class="ml-auto box-border">
+					<icon name="x" set="solid" class="h-6 w-6" />
+				</button>
+			</header>
+
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+
+	export default Vue.extend({
+		props: {
+			title: {
+				type: String,
+				default: '',
+			},
+		},
+	})
+</script>
