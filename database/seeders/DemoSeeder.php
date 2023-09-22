0ca3e617eedd6681077807b95e8d12a99d9e8d96
Brain Barett <brianbarett97@gmail.com>
2023-09-22 12:38:03 -0400

    [BE] wrap demo seeder in a transaction

--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Admin;
 use App\Models\Roles;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DemoSeeder extends Seeder
 {
@@ -15,17 +16,19 @@ class DemoSeeder extends Seeder
      */
     public function run()
     {
-        $admin = Admin::create([
-			'email' => 'admin@test.com',
-			'password' => 'password',
-			'language' => 'en',
-			'name' => 'Test Admin'
-		]);
-
-		$this->call([
-			DatabaseSeeder::class,
-		]);
-
-		$admin->assignRole(Roles::where('super_admin', true)->firstOrFail());
+		DB::transaction(function () {
+			$admin = Admin::create([
+				'email' => 'admin@test.com',
+				'password' => 'password',
+				'language' => 'en',
+				'name' => 'Test Admin'
+			]);
+	
+			$this->call([
+				DatabaseSeeder::class,
+			]);
+	
+			$admin->assignRole(Roles::where('super_admin', true)->firstOrFail());
+		});
     }
 }
