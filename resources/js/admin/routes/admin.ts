c1656cc6f0a06b328de37b5d4898ed7f9645b786
Brain Barett <brainbarett97@gmail.com>
2023-11-04 18:50:41 -0400

    [FE]feat: guard admin accounts and roles routes

--- resources/js/admin/routes/admin.ts
+++ resources/js/admin/routes/admin.ts
@@ -2,24 +2,34 @@ import { RouteConfig } from 'vue-router'
 import Create from '@/views/admin/Create.vue'
 import Index from '@/views/admin/Index.vue'
 import Edit from '@/views/admin/Edit.vue'
+import { User } from '@/stores/auth'
 
 const routes: RouteConfig[] = [
 	{
 		path: '/admin-accounts',
 		name: 'admin.index',
 		component: Index,
+		meta: {
+			authorize: (user: User) => user.isSuperAdmin,
+		},
 	},
 
 	{
 		path: '/admin-accounts/create',
 		name: 'admin.create',
 		component: Create,
+		meta: {
+			authorize: (user: User) => user.isSuperAdmin,
+		},
 	},
 
 	{
 		path: '/admin-accounts/:id',
 		name: 'admin.edit',
 		component: Edit,
+		meta: {
+			authorize: (user: User) => user.isSuperAdmin,
+		},
 	},
 ]
 
--- resources/js/admin/routes/roles.ts
+++ resources/js/admin/routes/roles.ts
@@ -2,24 +2,34 @@ import { RouteConfig } from 'vue-router'
 import Index from '@/views/roles/Index.vue'
 import Create from '@/views/roles/Create.vue'
 import Edit from '@/views/roles/Edit.vue'
+import { User } from '@/stores/auth'
 
 const routes: RouteConfig[] = [
 	{
 		path: '/roles',
 		name: 'roles.index',
 		component: Index,
+		meta: {
+			authorize: (user: User) => user.isSuperAdmin,
+		},
 	},
 
 	{
 		path: '/roles/create',
 		name: 'roles.create',
 		component: Create,
+		meta: {
+			authorize: (user: User) => user.isSuperAdmin,
+		},
 	},
 
 	{
 		path: '/roles/:id',
 		name: 'roles.edit',
 		component: Edit,
+		meta: {
+			authorize: (user: User) => user.isSuperAdmin,
+		},
 	},
 ]
 
