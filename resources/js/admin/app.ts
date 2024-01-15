dd6f81918a7b8f1cc5bf8a5149983abaa2988ee3
Brain Barett <brianbarett97@gmail.com>
2024-01-14 22:40:34 -0400

    [FE]refactor: move imports in app.ts to the top for readability

--- resources/js/admin/app.ts
+++ resources/js/admin/app.ts
@@ -1,55 +1,25 @@
 import Vue from 'vue'
-
 // @ts-ignore
 import VIcon from 'vue-tailwind-icons'
-Vue.use(VIcon)
-
 /** @ts-ignore */
 import CKEditor from '@ckeditor/ckeditor5-vue2'
-Vue.use(CKEditor)
-
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
-Vue.component('loading-spinner', LoadingSpinner)
-
 import 'floating-vue/dist/style.css'
 import FloatingVue from 'floating-vue'
-Vue.use(FloatingVue)
-
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
-Vue.use(Toast, {
-	icon: false,
-	draggable: false,
-	closeOnClick: false,
-	closeButton: false,
-	position: 'top-center',
-})
-
 import { createPinia, PiniaVuePlugin } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-Vue.use(PiniaVuePlugin)
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
-
 import VueRouter from 'vue-router'
-Vue.use(VueRouter)
-
 import VueI18n from 'vue-i18n'
 import { translations } from '@/lang'
-Vue.use(VueI18n)
-export const i18n = new VueI18n({
-	fallbackLocale: 'en',
-	messages: translations,
-})
-
 import VueFormulate, { FormulateGlobalInstance } from '@braid/vue-formulate'
 // @ts-ignore
 import { es } from '@braid/vue-formulate-i18n'
 import { formulateHandleApiPlugin } from './utils/formulate-handle-api'
-Vue.use(VueFormulate, {
-	plugins: [es, formulateHandleApiPlugin],
-	validationNameStrategy: ['validationName', 'label', 'name', 'type'],
-})
+import { permissions } from './permissions'
+import { router } from './router'
+import { useUiStore } from '@/stores/ui'
 
 /* augmentation can only be declared here @see https://github.com/vuejs/vue/issues/12642 */
 declare module 'vue/types/vue' {
@@ -58,10 +28,38 @@ declare module 'vue/types/vue' {
 	}
 }
 
-import { permissions } from './permissions'
+Vue.component('loading-spinner', LoadingSpinner)
+
+Vue.use(VIcon)
+Vue.use(CKEditor)
+Vue.use(FloatingVue)
+Vue.use(Toast, {
+	icon: false,
+	draggable: false,
+	closeOnClick: false,
+	closeButton: false,
+	position: 'top-center',
+})
+Vue.use(PiniaVuePlugin)
+Vue.use(VueRouter)
+Vue.use(VueI18n)
+Vue.use(VueFormulate, {
+	plugins: [es, formulateHandleApiPlugin],
+	validationNameStrategy: ['validationName', 'label', 'name', 'type'],
+})
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 Vue.prototype.$permissions = permissions
 
-import { router } from './router'
+export const i18n = new VueI18n({
+	fallbackLocale: 'en',
+	messages: translations,
+})
+
+useUiStore().setLanguage(useUiStore().language || 'en')
+
 export const app = new Vue({
 	el: '#app',
 	render: h => h('router-view'),
@@ -69,6 +67,3 @@ export const app = new Vue({
 	pinia,
 	i18n,
 })
-
-import { useUiStore } from '@/stores/ui'
-useUiStore().setLanguage(useUiStore().language || 'en')
